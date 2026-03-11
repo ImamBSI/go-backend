565e5aba7a22a81eb7348acbcd6a6a13117c3fad
Imam Brilliant Septa Ikhsandria <mambrilliant541@gmail.com>
2026-03-11 09:31:54 +0700

    refactor: add JSON tags to User and Account models for improved API response

--- internal/auth/model.go
+++ internal/auth/model.go
@@ -1,19 +1,19 @@
 package auth
 
 type User struct {
-	ID   uint   `gorm:"primaryKey"`
-	Name string `gorm:"column:name"`
-	Role string `gorm:"column:role"`
+	ID   uint   `gorm:"primaryKey" json:"id"`
+	Name string `gorm:"column:name" json:"name"`
+	Role string `gorm:"column:role" json:"role"`
 }
 
 func (User) TableName() string { return "User" }
 
 type Account struct {
-	ID       uint   `gorm:"primaryKey"`
-	Username string `gorm:"column:username;unique;not null"`
-	Password string `gorm:"column:password;not null"`
-	UserID   uint   `gorm:"column:user_id"`
-	User     User   `gorm:"foreignKey:UserID;references:ID"`
+	ID       uint   `gorm:"primaryKey" json:"id"`
+	Username string `gorm:"column:username;unique;not null" json:"username"`
+	Password string `gorm:"column:password;not null" json:"-"`
+	UserID   uint   `gorm:"column:user_id" json:"user_id"`
+	User     User   `gorm:"foreignKey:UserID;references:ID" json:"user"`
 }
 
 func (Account) TableName() string { return "Account" }
